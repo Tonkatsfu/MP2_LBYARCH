c998e89f98f4a26ae60e0e33e5dc23ee62ca5e83
JewelClaire <141259159+jewel-claire@users.noreply.github.com>
2024-11-25 16:28:01 +0800

    added calculation of execution time

--- mp2c.c
+++ mp2c.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <windows.h>
 
 extern void asmImgCvtGrayIntToDouble(int n, uint8_t* intImage, double* floatImage);
 
@@ -22,6 +23,31 @@ void printIntImage(uint8_t *image, int width, int height) {
         }
         printf("\n");
     }
+}
+
+void generateRandomImage(uint8_t *image, int width, int height) {
+    for (int i = 0; i < width * height; i++) {
+        image[i] = rand() % 256;  
+    }
+}
+
+void timeFunction(int width, int height, uint8_t* intImage, double* floatImage) {
+    LARGE_INTEGER start_time, end_time, frequency;
+    QueryPerformanceFrequency(&frequency);
+    double total_time = 0.0;
+    int num_runs = 30;
+
+    for (int i = 0; i < num_runs; i++) {
+        QueryPerformanceCounter(&start_time);
+        
+        asmImgCvtGrayIntToDouble(width * height, intImage, floatImage);
+
+        QueryPerformanceCounter(&end_time);
+        
+        total_time += (double)(end_time.QuadPart - start_time.QuadPart) / frequency.QuadPart;
+    }
+    
+    printf("\nAverage execution time for %dx%d image (%d runs): %.9f seconds\n", height, width, num_runs, total_time / num_runs);
 }
 
 int main() {
@@ -52,9 +78,34 @@ int main() {
     printf("\nConverted Float Image:\n");
     printDoubleImage(floatImage, width, height);
 
+    timeFunction(width, height, intImage, floatImage);
+
     free(intImage);
     free(floatImage);
 
+    // Calculate Average Execution Time for 10x10, 100x100, 1000x1000 Matrices
+    int sizes[][2] = {{10, 10}, {100, 100}, {1000, 1000}};
+    
+    for (int i = 0; i < 3; i++) {
+        width = sizes[i][0];
+        height = sizes[i][1];
+
+        uint8_t *intImage = (uint8_t *)malloc(width * height * sizeof(uint8_t));
+        double *floatImage = (double *)malloc(width * height * sizeof(double));
+
+        if (!intImage || !floatImage) {
+            printf("Memory allocation failed.\n");
+            return 1;
+        }
+
+        generateRandomImage(intImage, width, height);
+
+        timeFunction(width, height, intImage, floatImage);
+
+        free(intImage);
+        free(floatImage);
+    }
+    
     return 0;
 }
 
