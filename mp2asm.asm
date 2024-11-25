e99d00a53aac7799a45c33797ec70983ffad6706
JewelClaire <141259159+jewel-claire@users.noreply.github.com>
2024-11-25 16:24:55 +0800

    fixed to handle double precision

--- mp2asm.asm
+++ mp2asm.asm
@@ -1,5 +1,5 @@
 section .data
-val255 dd 255.0
+val255 dq 255.0
 
 section .text
 bits 64
@@ -9,7 +9,7 @@ global asmImgCvtGrayIntToDouble
 asmImgCvtGrayIntToDouble:
 
     mov r11, rcx ; save w*h, input counter
-    movss xmm2, [val255] ; divisor
+    movsd xmm2, [val255] ; divisor
     
     L1:
     ; check if no more inputs
@@ -18,17 +18,17 @@ asmImgCvtGrayIntToDouble:
 
     ; move value to SIMD register
         movzx rax, byte [rdx] ; zero extend uint8_t input
-        cvtsi2ss xmm1, rax
+        cvtsi2sd xmm1, rax
         
     ; divide
-        divss xmm1, xmm2
+        divsd xmm1, xmm2
         
     ; store answer
-        movss [r8], xmm1
+        movsd [r8], xmm1
         
     ; shift pointers
         inc rdx ; +1 since intImage input is only 1 byte
-        add r8, 4 ; move to next element in floatImage
+        add r8, 8 ; move to next element in floatImage
         dec r11
         jmp L1
         
